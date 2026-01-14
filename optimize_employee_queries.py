e9b10c3bc12e58d153fa515f2215a38dd2254c7d
salmon raju <salmonrajudasaris@gmail.com>
2026-01-14 12:53:59 +0530

    new changes

--- optimize_employee_queries.py
+++ optimize_employee_queries.py
@@ -11,11 +11,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # PostgreSQL database configuration
-POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "admin")
+POSTGRES_USER = os.getenv("POSTGRES_USER", "pos_inventory")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "9491316460Aa!")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "supermarket")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "pos_inventory")
 
 # Create PostgreSQL database URL
 DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
